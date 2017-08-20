3169bf323af1e696c0697d1d445444a03e6ae4c8
saad boudfor <sboudfor@gmail.com>
2017-08-20 15:28:13 +0200

    Add spinner and curd operation

---
+++ src/app/transvers/transvers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transvers',
+  templateUrl: './transvers.component.html',
+  styleUrls: ['./transvers.component.css']
+})
+export class TransversComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
